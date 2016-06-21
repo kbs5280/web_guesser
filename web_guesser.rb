5864a928e77bcaf646c1546079d2756b0bde8faa
Kris Sparks <kbs5280@gmail.com>
2016-06-21 16:46:21 -0600

    Extension: Guess Limiting working along with some corrections, working per spec

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,18 +2,26 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+@@secret_number = rand(100)
+@@num_of_guesses = 5
 
   get '/' do
     guess = params["guess"]
     message = check_guess(guess)
-    erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+    erb :index, :locals => {:number => @@secret_number, :message => message}
   end
 
   def check_guess(guess)
-    number = SECRET_NUMBER
+    number = @@secret_number
     guess = guess.to_i
+    @@num_of_guesses -= 1
     case
+    when @@num_of_guesses == 0
+      reset
+      "You lose! Try again."
+    when guess == 0
+      @@num_of_guesses += 1
+      "Please enter a guess: "
     when guess > number + 5
       "Way too high!"
     when guess > number
@@ -23,7 +31,14 @@ SECRET_NUMBER = rand(100)
     when guess < number
       "Too low!"
     when guess == number
+      correct_number = number
+      reset
       "You got it right!"
-      "The SECRET NUMBER is #{SECRET_NUMBER}"
+      "The SECRET NUMBER is #{number}"
     end
+  end
+
+  def reset
+    @@num_of_guesses = 5
+    @@secret_number = rand(100)
   end
